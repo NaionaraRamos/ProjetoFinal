8087d7861bc88f73a6b1777b1757ab5058ad775d
AnaCarolinaFortunato <anacarolinafortunatosantos@gmail.com>
2021-02-12 21:53:45 -0300

    commit inicial

---
+++ src/Modules/InstaGama.Domain/Entities/Album.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    public class Album
+    {
+        public int MyProper { get; set; }
+    }
+}
---
+++ src/Modules/InstaGama.Domain/Entities/Relationship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaGama.Domain.Entities
+{
+    
+    public class Relationship
+        
+    {
+        public Relationship(int id,
+                            int userId,
+                            int friendId)
+
+        {
+            Id = id;
+            UserId = userId;
+            FriendId = friendId;
+        }
+
+        
+        public int Id { get; private set; }
+
+        public int UserId { get; private set; }
+
+        public int FriendId { get; private set; }
+    }
+
+}
